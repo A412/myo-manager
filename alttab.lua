81fcd068dd9b79af17fc1ebb3716b7fbeedf770c
A412 <max.mf.feng@gmail.com>
2014-10-04 06:23:45 -0700

    Update alttab.lua

--- alttab.lua
+++ alttab.lua
@@ -1,10 +1,11 @@
 function onPoseEdge(pose,edge)
+	myo.keyboard("left_alt","down")
 	myo.keyboard("tab","press")
+	myo.keyboard("left_alt","up")
 end
 
 function onForegroundWindowChange(app, title)
     if string.match(title, title) then
-    	myo.keyboard("alt","down")
 		return true
 	end
 end
