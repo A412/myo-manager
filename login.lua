da8e8c8708cbf41d1e18f10f78a80b8577191d69
A412 <max.mf.feng@berkeley.edu>
2014-10-04 08:30:09 -0700

    state-based

--- login.lua
+++ login.lua
@@ -32,26 +32,123 @@ end
 
 function unlock()
 	unlocked = true
-	myo.vibrate("short")
-	myo.vibrate("short")
 	extendunlock()
 end
 
 function lock()
-	unlocked = false
-	myo.vibrate("long")
+	unlocked = false)
+end
+
+function buzz(num)
+	if num == 0 then
+		myo.vibrate("long")
+	else
+		for vibration_num = 1,num,1 do
+			myo.vibrate("short")
+		end
+	end
 end
 
 -- Interaction
 
+state = 0
+
 function onPoseEdge(pose,edge)
 	extendunlock()
+	if not state == 2 and myo.mouseControlEnabled() then
+		myo.controlMouse(false)
+	end
+	if state == 0 then --locked
+		buzz(state)
+		if pose == "fist" then
+	        if edge == "off" then
+	            change_state()
+	        elseif edge == "on" and not unlocked then
+	           	extendunlock()
+           	end
+        end
+	elseif state == 1 then --main menu
+		buzz(state)
+		if pose == "fist" then
+	        if edge == "off" then
+	            change_state()
+	        elseif edge == "on" and not unlocked then
+	           	extendunlock()
+           	end
+        elseif pose == "fingersSpread" and edge == "off" then
+        	state = 2
+	elseif state == 2 then --mousemode
+		buzz(state)
+		if pose == "fist" then
+	        if edge == "off" then
+	            change_state()
+	        elseif edge == "on" and not unlocked then
+	           	extendunlock()
+           	end
+       	else
+       		if not myo.mouseControlEnabled() then
+				myo.controlMouse(true)
+			if pose == "waveIn" and unlocked then
+				if edge == "on" then
+					myo.mouse("left","down")
+				else
+					myo.mouse("left","up")
+				end
+			elseif pose == "waveOut" and unlocked then
+				if edge == "on" then
+					myo.mouse("right","down")
+				else
+					myo.mouse("right","up")
+				end
+			elseif pose == "fingersSpread" and unlocked then
+				if edge == "on" then
+					myo.mouse("center","down")
+				else
+					myo.mouse("center","up")
+				end
+			end
+		end
+	end
+	elseif state == 3 then --windows commands
+		if myo.mouseControlEnabled() then
+			myo.controlMouse(false)
+		end
+		buzz(state)
+		if pose == "fist" then
+	        if edge == "off" then
+	            change_state()
+	        elseif edge == "on" and not unlocked then
+	           	extendunlock()
+           	end
+        end
+	elseif state == 4 then --power options
+		if myo.mouseControlEnabled() then
+			myo.controlMouse(false)
+		end
+		buzz(state)
+		if pose == "fist" then
+	        if edge == "off" then
+	            change_state()
+	        elseif edge == "on" and not unlocked then
+	           	extendunlock()
+           	end
+        end
+    elseif state == 5 then --custom binds
+    	if myo.mouseControlEnabled() then
+			myo.controlMouse(false)
+		end
+    	buzz(state)
+    	if pose == "fist" then
+	        if edge == "off" then
+	            change_state()
+	        elseif edge == "on" and not unlocked then
+	           	extendunlock()
+           	end
+        end
 	if pose == "fist" then
         if edge == "off" then
             change_state()
         elseif edge == "on" and not unlocked then
-        	myo.vibrate("short")
-           	myo.vibrate("short")
            	extendunlock()
         end
     elseif not myo.mouseControlEnabled() then
