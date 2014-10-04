9b3f3e3ce8b91570d9ff3b824a4ea07e4ff8651b
A412 <max.mf.feng@gmail.com>
2014-10-04 05:21:19 -0700

    Create login.lua

---
+++ login.lua
@@ -0,0 +1,53 @@
+scriptId = 'com.cmv.login'
+
+function keypress(key)
+	myo.keyboard(key,"press")
+end
+
+function lockwindows()
+	--myo.keyboard("left_win","down")
+	--myo.keyboard("win","down")
+	--myo.keyboard("l","press")
+	--myo.keyboard("win","up")
+	myo.keyboard("f11", "press")
+	--myo.keyboard("left_win","up")
+end
+
+UNLOCKED_TIMEOUT = 5000
+
+function extendunlock()
+	unlockedSince = myo.getTimeMilliseconds()
+end
+
+function unlock()
+	unlocked = true
+	myo.vibrate("short")
+	myo.vibrate("short")
+	extendunlock()
+end
+
+function lock()
+	unlocked = false
+	myo.vibrate("long")
+end
+
+function onPoseEdge(pose,edge)
+	extendunlock()
+	if unlocked == true then
+		if pose == "waveOut" then
+			lockwindows()
+		end
+		if pose == "fist" then
+			myo.keyboard("space","press")
+		end
+	end
+	if unlocked == false and pose == "fist" then
+		unlock()
+	end
+end
+
+function onForegroundWindowChange(app, title)
+    if string.match(title, title) then
+		return true
+	end
+end
